045cd6f258f68d6d73f1c55dd139394f3ec470af
this-is-tobi <thibault_colin@hotmail.fr>
2024-05-18 05:31:52 +0200

    refactor: improve nginx docker template

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
-ROOT_DIR=/opt/bitnami/nginx
-ENV_FILE=/env/.env
+ROOT_DIR=/opt/bitnami/nginx/html
+# Prefix that is used to find variables in js files 
+# ex: `const test = provess.env.SERVER_HOST || 'nginx-server-host'`
+KEY_PREFIX=nginx
+# List of variables to inject into js files from environment eval
+VARIABLES=(
+  # SERVER_HOST
+  # SERVER_PORT
+  # KEYCLOAK_DOMAIN
+  # KEYCLOAK_REALM
+  # KEYCLOAK_CLIENT_ID
+  # KEYCLOAK_REDIRECT_URI
+)
 
 populate () {
-  if [ -z $(eval "echo \${$1}") ]; then
-    VAR="$(grep "^$1" "$ENV_FILE" | xargs)"
-    if [ ! -z "$VAR" ]; then
-      export "$(grep "^$1" "$ENV_FILE" | xargs)"
-    fi
-  fi
-
-  KEY="\$${1}"
+  KEY=$(echo "$KEY_PREFIX-$1" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g')
   VALUE=$(eval "echo \${$1}")
   sed -i 's|'${KEY}'|'${VALUE}'|g' $2
 }
 
-echo "Replacing env constants in JS assets"
-for file in ${ROOT_DIR%/}/html/assets/*.js; do
-  echo "Processing $file ..."
 
-  cat "$ENV_FILE" | while read e; do
-    [[ "${e:0:1}" == "#" ]] && continue
-    populate "$(echo "$e" | cut -d "=" -f 1)" $file
+echo "Replacing env constants in JS"
+for file in $ROOT_DIR/assets/*.js; do
+  echo "Processing $file ...";
+  for var in ${VARIABLES[*]}; do
+    populate $var $file
   done
 done
-
-sed -i 's|$SERVER|'${SERVER}'|g' ${ROOT_DIR%/}/conf/server_blocks/default.conf
 
 nginx -g 'daemon off;'
