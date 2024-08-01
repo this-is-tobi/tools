2c2f248b5ac2896d298cfec8b66c2c1ad81894a7
this-is-tobi <this-is-tobi@proton.me>
2024-08-01 13:12:39 +0200

    refactor: fix wording in backup scripts

--- docker/pg-backup/backup.sh
+++ docker/pg-backup/backup.sh
@@ -29,7 +29,7 @@ printf "\n\nBackup finished\n\n"
 
 # Delete backups older than
 if [ ! -z "${RETENTION}" ]; then
-  printf "\n\nDelete backups older than ${RETENTION}days in '${S3_BUCKET_NAME}${S3_BUCKET_PREFIX:+/}${S3_BUCKET_PREFIX}'\n\n"
+  printf "\n\nDelete backups older than ${RETENTION} in '${S3_BUCKET_NAME}${S3_BUCKET_PREFIX:+/}${S3_BUCKET_PREFIX}'\n\n"
 
   mc ${MC_EXTRA_ARGS} rm --recursive --force --older-than "${RETENTION}" backup_host/${S3_BUCKET_NAME%/}${S3_BUCKET_PREFIX:+/}${S3_BUCKET_PREFIX}
 fi
