44432c19d96e8d6a4a8a9a5c836db180b7e14d65
this-is-tobi <this-is-tobi@proton.me>
2025-11-21 18:54:41 +0100

    refactor: improve keycloak-required-tac shell script

--- shell/keycloak-required-tac.sh
+++ shell/keycloak-required-tac.sh
@@ -11,6 +11,8 @@ COLOR_YELLOW='\033[0;33m'
 
 # Defaults
 KC_USERNAME="admin"
+KC_FORCE_ALL=false
+KC_BATCH_SIZE=100
 
 # Script helper
 TEXT_HELPER="
@@ -22,14 +24,28 @@ Available flags:
   -r    Keycloak realm where to list users.
   -u    Keycloak username.
         Default: '$KC_USERNAME'.
+  -b    Batch size for pagination (number of users per request).
+        Default: '$KC_BATCH_SIZE'.
+  -f    Force all users to accept T&C (even those who already accepted).
+        Use this when T&C have been updated.
+        Default: false (only users who haven't accepted).
   -h    Print script help.
 
 Example:
-  ./keycloak-list-users.sh \\
+  # Force only users who haven't accepted T&C
+  ./keycloak-required-tac.sh \\
     -k 'http://localhost:8080' \\
     -p 'admin' \\
     -r 'my-realm' \\
     -u 'admin'
+
+  # Force ALL users (e.g., after T&C update)
+  ./keycloak-required-tac.sh \\
+    -k 'http://localhost:8080' \\
+    -p 'admin' \\
+    -r 'my-realm' \\
+    -u 'admin' \\
+    -f
 "
 
 # Functions
@@ -38,7 +54,7 @@ print_help() {
 }
 
 # Parse options
-while getopts hk:p:r:u: flag; do
+while getopts b:fhk:p:r:u: flag; do
   case "${flag}" in
     k)
       KC_HOST=${OPTARG};;
@@ -48,6 +64,10 @@ while getopts hk:p:r:u: flag; do
       KC_REALM=${OPTARG};;
     u)
       KC_USERNAME=${OPTARG};;
+    b)
+      KC_BATCH_SIZE=${OPTARG};;
+    f)
+      KC_FORCE_ALL=true;;
     h | *)
       print_help
       exit 0;;
@@ -60,6 +80,8 @@ Settings:
   > KC_HOST: ${KC_HOST}
   > KC_REALM: ${KC_REALM}
   > KC_USERNAME: ${KC_USERNAME}
+  > KC_BATCH_SIZE: ${KC_BATCH_SIZE}
+  > KC_FORCE_ALL: ${KC_FORCE_ALL}
 "
 
 # Options validation
@@ -82,18 +104,87 @@ ACCESS_TOKEN=$(curl -fsSL \
   -d "password=$KC_PASSWORD" \
   -d "grant_type=password" | jq -r '.access_token')
 
-USERS=$(curl -fsSL \
-  -X GET "$KC_HOST/admin/realms/$KC_REALM/users?max=-1" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $ACCESS_TOKEN" | jq -c '.')
+# Get total user count first
+TOTAL_USERS=$(curl -fsSL \
+  -X GET "$KC_HOST/admin/realms/$KC_REALM/users/count" \
+  -H "Authorization: Bearer $ACCESS_TOKEN")
 
-# Force users to accept terms and conditions
-for USER in $(echo "$USERS" | jq -c '.[]'); do
-  printf "Processing user : "
-  printf "$USER" | jq '.username'
-  curl -fsSL \
-    -X PUT "$KC_HOST/admin/realms/$KC_REALM/users/$(echo $USER | jq -r '.id')" \
+printf "\n${COLOR_BLUE}Total users in realm:${COLOR_OFF} $TOTAL_USERS\n"
+
+# Process users in batches
+UPDATED_COUNT=0
+SKIPPED_COUNT=0
+PROCESSED_COUNT=0
+FIRST=0
+
+while true; do
+  BATCH_END=$((FIRST + KC_BATCH_SIZE - 1))
+  if [ "$BATCH_END" -gt "$TOTAL_USERS" ]; then
+    BATCH_END=$TOTAL_USERS
+  fi
+  
+  printf "\n${COLOR_BLUE}Fetching users ${FIRST}-${BATCH_END}...${COLOR_OFF}\n"
+  
+  USERS=$(curl -fsSL \
+    -X GET "$KC_HOST/admin/realms/$KC_REALM/users?first=$FIRST&max=$KC_BATCH_SIZE" \
     -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -d '{"requiredActions": ["TERMS_AND_CONDITIONS"]}'
+    -H "Authorization: Bearer $ACCESS_TOKEN" | jq -c '.')
+  
+  BATCH_COUNT=$(echo "$USERS" | jq 'length')
+  
+  # Exit if no more users
+  if [ "$BATCH_COUNT" -eq 0 ]; then
+    break
+  fi
+
+  for USER in $(echo "$USERS" | jq -c '.[]'); do
+    USERNAME=$(echo "$USER" | jq -r '.username')
+    USER_ID=$(echo "$USER" | jq -r '.id')
+    ATTRIBUTES=$(echo "$USER" | jq -r '.attributes // {}')
+    HAS_ACCEPTED_TAC=$(echo "$ATTRIBUTES" | jq 'has("terms_and_conditions")')
+    REQUIRED_ACTIONS=$(echo "$USER" | jq -r '.requiredActions // []')
+    HAS_TAC_PENDING=$(echo "$REQUIRED_ACTIONS" | jq 'any(. == "TERMS_AND_CONDITIONS")')
+    
+    if [ "$KC_FORCE_ALL" = true ]; then
+      # Force all users to accept T&C (even those who already accepted)
+      printf "${COLOR_BLUE}Processing user:${COLOR_OFF} $USERNAME (force all mode)\n"
+      curl -fsSL \
+        -X PUT "$KC_HOST/admin/realms/$KC_REALM/users/$USER_ID" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $ACCESS_TOKEN" \
+        -d '{"requiredActions": ["TERMS_AND_CONDITIONS"]}'
+      UPDATED_COUNT=$((UPDATED_COUNT + 1))
+    elif [ "$HAS_ACCEPTED_TAC" = false ] && [ "$HAS_TAC_PENDING" = false ]; then
+      # User has NOT accepted T&C and doesn't have pending action
+      printf "${COLOR_YELLOW}Processing user:${COLOR_OFF} $USERNAME (user never accepted T&C)\n"
+      
+      # Get current required actions and add TERMS_AND_CONDITIONS
+      NEW_ACTIONS=$(echo "$REQUIRED_ACTIONS" | jq '. + ["TERMS_AND_CONDITIONS"] | unique')
+      
+      curl -fsSL \
+        -X PUT "$KC_HOST/admin/realms/$KC_REALM/users/$USER_ID" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $ACCESS_TOKEN" \
+        -d "{\"requiredActions\": $NEW_ACTIONS}"
+      UPDATED_COUNT=$((UPDATED_COUNT + 1))
+    else
+      # User has already accepted T&C or has pending action
+      if [ "$HAS_ACCEPTED_TAC" = true ]; then
+        printf "${COLOR_GREEN}Skipping user:${COLOR_OFF} $USERNAME (already accepted T&C)\n"
+      else
+        printf "${COLOR_GREEN}Skipping user:${COLOR_OFF} $USERNAME (T&C already pending)\n"
+      fi
+      SKIPPED_COUNT=$((SKIPPED_COUNT + 1))
+    fi
+    PROCESSED_COUNT=$((PROCESSED_COUNT + 1))
+  done
+  
+  # Move to next batch
+  FIRST=$((FIRST + KC_BATCH_SIZE))
 done
+
+printf "\n${COLOR_GREEN}Summary:${COLOR_OFF}\n"
+printf "  - Total users in realm: $TOTAL_USERS\n"
+printf "  - Processed: $PROCESSED_COUNT\n"
+printf "  - Updated: $UPDATED_COUNT\n"
+printf "  - Skipped: $SKIPPED_COUNT\n"
